dd517cf652aa4a86224688ec45aa7a2c174e0424
makemoremoney <363945080@qq.com>
2016-12-08 23:16:49 +0800

    微信登录

---
+++ app/src/main/java/com/zfwl/common/Const.java
@@ -0,0 +1,11 @@
+package com.zfwl.common;
+
+/**
+ * Created by ZZB on 2016/12/8.
+ */
+public class Const {
+
+    public interface WeChatLogin {
+        String APP_ID = "";
+    }
+}
---
+++ app/src/main/java/com/zfwl/wxapi/WXEntryActivity.java
@@ -0,0 +1,15 @@
+package com.zfwl.wxapi;
+
+import android.os.Bundle;
+
+import com.zfwl.R;
+import com.zfwl.activity.BaseActivity;
+
+public class WXEntryActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxentry);
+    }
+}
