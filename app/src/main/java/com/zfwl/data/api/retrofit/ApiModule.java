4af697f6fa617f008ec27636cdad6fabf012a57f
makemoremoney <363945080@qq.com>
2016-12-09 22:43:51 +0800

    okhttp 配置

--- app/src/main/java/com/zfwl/data/api/retrofit/ApiModule.java
+++ app/src/main/java/com/zfwl/data/api/retrofit/ApiModule.java
@@ -1,9 +1,14 @@
 package com.zfwl.data.api.retrofit;
 
 import com.google.gson.Gson;
+import com.zfwl.common.MyLog;
 import com.zfwl.data.api.LoginApi;
 import com.zfwl.data.api.SignUpApi;
+
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -18,7 +23,9 @@ public class ApiModule {
 
     public static ApiModule INSTANCE = new ApiModule();
     private static final String BASE_URL = "http://139.129.218.83:90/logistics/";
+    private static final int CONNECT_TIMEOUT = 15;
     private Retrofit mRetrofit;
+    private OkHttpClient mOkHttpClient;
 
     private ApiModule() {
     }
@@ -38,12 +45,38 @@ public class ApiModule {
     public LoginApi provideLoginApi() {
         return ApiModule.INSTANCE.provideRetrofit().create(LoginApi.class);
     }
+
     public SignUpApi provideSignUpApi() {
         return ApiModule.INSTANCE.provideRetrofit().create(SignUpApi.class);
     }
 
 
     private OkHttpClient provideOkHttpClient() {
-        return new OkHttpClient();
+        if (mOkHttpClient == null) {
+            initOkHttpClient();
+        }
+        return mOkHttpClient;
     }
+
+    private void initOkHttpClient() {
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.retryOnConnectionFailure(true)
+                .addInterceptor(provideLoggingInterceptor())
+                .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                .build();
+        mOkHttpClient = builder.build();
+    }
+
+    private HttpLoggingInterceptor provideLoggingInterceptor() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                MyLog.i("OkHttp", message);
+            }
+        });
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return interceptor;
+    }
+
 }
