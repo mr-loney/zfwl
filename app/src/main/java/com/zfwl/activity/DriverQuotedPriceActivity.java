2ef0bb9d154cf3ccc0aafda20c3387fc0e55455d
makemoremoney <363945080@qq.com>
2016-12-19 00:02:04 +0800

    司机报价

--- app/src/main/java/com/zfwl/activity/DriverQuotedPriceActivity.java
+++ app/src/main/java/com/zfwl/activity/DriverQuotedPriceActivity.java
@@ -4,10 +4,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.zfwl.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.bingoogolapple.titlebar.BGATitleBar;
+
 public class DriverQuotedPriceActivity extends AppCompatActivity {
+
+    @BindView(R.id.title_bar)
+    BGATitleBar mTitleBar;
+    @BindView(R.id.tv_price_by_weight)
+    TextView mTvPriceByWeight;
+    @BindView(R.id.tv_price_by_car)
+    TextView mTvPriceByCar;
+    @BindView(R.id.tv_price_total)
+    TextView mTvPriceTotal;
+    @BindView(R.id.tv_car_num)
+    TextView mTvCarNum;
 
     public static void launch(Context context) {
         Intent intent = new Intent(context, DriverQuotedPriceActivity.class);
@@ -18,5 +34,6 @@ public class DriverQuotedPriceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_quoted_price);
+        ButterKnife.bind(this);
     }
 }
