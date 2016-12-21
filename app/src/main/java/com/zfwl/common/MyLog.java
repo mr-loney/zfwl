fe0ee2e533895733394825385d59c09052fa5196
makemoremoney <363945080@qq.com>
2016-12-21 22:10:57 +0800

    log

--- app/src/main/java/com/zfwl/common/MyLog.java
+++ app/src/main/java/com/zfwl/common/MyLog.java
@@ -9,21 +9,37 @@ import timber.log.Timber;
  */
 
 public class MyLog {
-    public static void init(){
+    public static void init() {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
     }
-    public static void i(String tag, String msg, Object... args){
+
+    public static void i(String tag, String msg, Object... args) {
         Timber.tag(tag).i(msg, args);
     }
-    public static void e(String tag, Throwable throwable, String msg, Object... args){
+
+    public static void d(String tag, String msg, Object... args) {
+        Timber.tag(tag).d(msg, args);
+    }
+
+    public static void w(String tag, String msg, Object... args) {
+        Timber.tag(tag).w(msg, args);
+    }
+
+    public static void v(String tag, String msg, Object... args) {
+        Timber.tag(tag).v(msg, args);
+    }
+
+    public static void e(String tag, Throwable throwable, String msg, Object... args) {
         Timber.tag(tag).e(throwable, msg, args);
     }
-    public static void e(String tag, String msg, Object... args){
+
+    public static void e(String tag, String msg, Object... args) {
         Timber.tag(tag).e(msg, args);
     }
-    public static void e(String tag, Throwable throwable){
+
+    public static void e(String tag, Throwable throwable) {
         Timber.tag(tag).e(throwable);
     }
 }
