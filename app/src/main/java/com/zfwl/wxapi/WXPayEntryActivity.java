7c8d54493aa741c5e640f35c189b448a23e193ed
mmmoney <363945080@qq.com>
2017-01-17 17:28:09 +0800

    pay

---
+++ app/src/main/java/com/zfwl/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,15 @@
+package com.zfwl.wxapi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zfwl.R;
+
+public class WXPayEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxpay_entry);
+    }
+}
