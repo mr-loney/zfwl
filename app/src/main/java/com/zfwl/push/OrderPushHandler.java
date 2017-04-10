934448ab03a5c8e250b1cbd7424dcba38c59254d
mmmoney <363945080@qq.com>
2017-04-10 22:50:10 +0800

    推送bug

--- app/src/main/java/com/zfwl/push/OrderPushHandler.java
+++ app/src/main/java/com/zfwl/push/OrderPushHandler.java
@@ -4,21 +4,14 @@ import android.content.Context;
 
 import com.zfwl.activity.GoodsDetailActivity;
 import com.zfwl.activity.QuotedPriceDetailActivity;
-import com.zfwl.activity.myorders.detail.CarryingOrderDetailActivity;
-import com.zfwl.activity.myorders.detail.FinishedOrderDetailActivity;
-import com.zfwl.activity.myorders.detail.PaidOrderDetailActivity;
-import com.zfwl.activity.myorders.detail.WaitCommentDetailActivity;
 import com.zfwl.activity.myorders.detail.WaitConfirmOrderDetailActivity;
-import com.zfwl.activity.myorders.detail.WaitPayOrderDetailActivity;
 import com.zfwl.common.MyLog;
-import com.zfwl.entity.Order.Type;
 import com.zfwl.event.OrderPushEvent;
 import com.zfwl.push.data.NewLogicInfo;
 import com.zfwl.push.data.OrderPushContent;
 import com.zfwl.push.data.PushData;
 import com.zfwl.push.data.QuotedCarRunOut;
 import com.zfwl.util.GsonUtils;
-import com.zfwl.widget.ToastUtils;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -82,29 +75,30 @@ public class OrderPushHandler {
         OrderPushContent content = extraToOrder(extra);
         int orderType = content.orderType;
         long orderId = content.orderId;
-        switch (orderType) {
-            case Type.WAIT_CONFIRM:
-                WaitConfirmOrderDetailActivity.launch(context, orderId, true);
-                break;
-            case Type.PAID:
-                PaidOrderDetailActivity.launch(context, orderId, true);
-                break;
-            case Type.WAIT_PAY:
-                WaitPayOrderDetailActivity.launch(context, orderId, true);
-                break;
-            case Type.CARRYING:
-                CarryingOrderDetailActivity.launch(context, orderId, true);
-                break;
-            case Type.WAIT_COMMENT:
-                WaitCommentDetailActivity.launch(context, orderId, true);
-                break;
-            case Type.COMMENTED:
-                FinishedOrderDetailActivity.launch(context, orderId, true);
-                break;
-            default:
-                ToastUtils.show(context, "未知订单：" + orderId);
-                break;
-        }
+        WaitConfirmOrderDetailActivity.launch(context, orderId, true);
+//        switch (orderType) {
+//            case Type.WAIT_CONFIRM:
+//                WaitConfirmOrderDetailActivity.launch(context, orderId, true);
+//                break;
+//            case Type.PAID:
+//                PaidOrderDetailActivity.launch(context, orderId, true);
+//                break;
+//            case Type.WAIT_PAY:
+//                WaitPayOrderDetailActivity.launch(context, orderId, true);
+//                break;
+//            case Type.CARRYING:
+//                CarryingOrderDetailActivity.launch(context, orderId, true);
+//                break;
+//            case Type.WAIT_COMMENT:
+//                WaitCommentDetailActivity.launch(context, orderId, true);
+//                break;
+//            case Type.COMMENTED:
+//                FinishedOrderDetailActivity.launch(context, orderId, true);
+//                break;
+//            default:
+//                ToastUtils.show(context, "未知订单：" + orderId);
+//                break;
+//        }
     }
 
     private static int getPushType(String json) {
