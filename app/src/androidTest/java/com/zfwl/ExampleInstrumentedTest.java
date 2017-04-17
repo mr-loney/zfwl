29d9d1d313ca946730326e3b947d77d5902bb87d
帅哥 <mr_lonely@foxmail.com>
2017-04-17 14:50:55 +0800

    改包名

--- app/src/androidTest/java/com/zfwl/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/zfwl/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.zfwl", appContext.getPackageName());
+        assertEquals("com.zfwl.official", appContext.getPackageName());
     }
 }
